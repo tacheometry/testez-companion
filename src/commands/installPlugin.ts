9354ab9ba0f957182383a4321588e4c125571d6d
tacheometry <39647014+tacheometry@users.noreply.github.com>
2021-04-30 20:20:34 +0300

    darwin support for installing plugins

--- src/commands/installPlugin.ts
+++ src/commands/installPlugin.ts
@@ -1,29 +1,57 @@
 import * as vscode from "vscode";
+import openExplorer from "open-file-explorer";
 import * as path from "path";
 import * as fs from "fs";
 import * as os from "os";
 
+function copyPlugin(source: string, destination: string) {
+	fs.copyFile(source, destination, () => {
+		vscode.window
+			.showInformationMessage(
+				`Successfully copied the plugin to ${destination}`,
+				"View file"
+			)
+			.then((clicked) => {
+				if (clicked === "View file")
+					try {
+						openExplorer(destination, () => {});
+					} catch {}
+			});
+	});
+}
+
 export const installPlugin = () => {
+	const pluginPath = path.join(
+		__dirname,
+		"..",
+		"plugin",
+		"TestEZ Companion.rbxmx"
+	);
+
 	switch (os.platform()) {
 		case "win32":
-			const destination = path.join(
-				process.env["LOCALAPPDATA"]!,
-				"Roblox",
-				"Plugins",
-				"TestEZ Companion.rbxmx"
-			);
-			const source = path.join(
-				__dirname,
-				"..",
-				"plugin",
-				"TestEZ Companion.rbxmx"
+			copyPlugin(
+				pluginPath,
+				path.join(
+					process.env["LOCALAPPDATA"]!,
+					"Roblox",
+					"Plugins",
+					"TestEZ Companion.rbxmx"
+				)
 			);
 
-			fs.copyFile(source, destination, () => {
-				vscode.window.showInformationMessage(
-					"Successfully copied the plugin to %LOCALAPPDATA%/Roblox/Plugins/TestEZ Companion.rbxmx"
-				);
-			});
+			break;
+		case "darwin":
+			copyPlugin(
+				pluginPath,
+				path.join(
+					process.env["HOME"]!,
+					"Documents",
+					"Roblox",
+					"Plugins",
+					"TestEZ Companion.rbxmx"
+				)
+			);
 
 			break;
 		default:
